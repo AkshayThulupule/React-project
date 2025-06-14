c8cc62517f0bf2a8b74033de9e5323ead515ab74
Panchami Thulupule <62180655+panchami6@users.noreply.github.com>
2025-06-14 19:11:16 +0530

    counter app

---
+++ my-app/src/Counter.js
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+const Counter = () => {
+    const [counter, setCounter] = useState(0)
+    const [inputValue, setInputValue] = useState('')
+
+    return (
+        <>
+        <button onClick = {() => setCounter(counter+1)}>Increment</button>
+        <button onClick = {() => setCounter(counter-1)}>Decrement</button>
+        <input onChange = {(e)=> setInputValue(e.target.value)}/>
+        <div>{counter}</div>
+        <div>{inputValue}</div>
+        </>
+    )
+}
+
+export default Counter
